45ac1006ec0e6c8a310d7d60436f64894fece84d
Marie Duchene <marie_duchene44@hotmail.com>
2018-02-01 00:04:54 -0500

    stuff

---
+++ HW/p5/hw2/sketch.js
@@ -0,0 +1,14 @@
+var centerX = 100;
+var centerY = 200;
+
+createCanvas(800,800);
+background(255,109,109);
+}
+
+function setup() {
+  // put setup code here
+}
+
+function draw() {
+  // put drawing code here
+}
