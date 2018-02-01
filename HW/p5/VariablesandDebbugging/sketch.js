233283b1c70cbc6eafb3e1af174909a2471d4eaf
Marie Duchene <marie_duchene44@hotmail.com>
2018-02-01 12:52:18 -0500

    another

---
+++ HW/p5/VariablesandDebbugging/sketch.js
@@ -0,0 +1,18 @@
+var number1 = 1;
+var number2 = 2;
+var letter1 = 'a';
+var letter2 = 'b';
+var greeting = "hello";
+var greeting2 = "goodbye";
+var truthordare = true;
+var truthordare2 = false;
+
+function setup() {
+  // put setup code here
+
+}
+
+function draw() {
+  // put drawing code here\
+
+}
--- HW/p5/hw2/sketch.js
+++ HW/p5/hw2/sketch.js
@@ -1,12 +1,13 @@
 var centerX = 100;
 var centerY = 200;
 
-createCanvas(400,400);
-background(255);
-}
 
 function setup() {
   // put setup code here
+
+  createCanvas(400,400);
+  background(255);
+
   background(255,109,109);
   centerX = mouseX;
   centerY = mouseY;
@@ -16,4 +17,29 @@ function setup() {
 
 function draw() {
   // put drawing code here
+
+  background(252,179,176);
+  centerX = mouseX;
+  centerY = mouseY;
+  stroke(0);
+  strokeWeight(1);
+
+  fill("white");
+  rect(centerX -150,centerY -100,100,100);
+  ellipse(centerX-100,centerY-50,50);
+  rect(centerX + 25,centerY -100,100,100);
+  ellipse(centerX +75,centerY -50,50,75);
+
+  stroke(125);
+  strokeWeight(5);
+  point(centerX, centerY);
+
+  stroke(255,0,0);
+  line(centerX-50,centerY+50,centerX +50, centerY+50);
+
+  noStroke();
+  fill(0,0,255);
+  triangle(centerX,centerY,centerX +25, centerY +25, centerX -25, centerY +25);
+
+
 }
