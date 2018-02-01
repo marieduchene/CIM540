4ad7fc19ddbb586ece903d92476e4381c216b278
Marie Duchene <marie_duchene44@hotmail.com>
2018-02-01 00:14:12 -0500

    things

--- HW/p5/hw2/sketch.js
+++ HW/p5/hw2/sketch.js
@@ -7,6 +7,11 @@ background(255);
 
 function setup() {
   // put setup code here
+  background(255,109,109);
+  centerX = mouseX;
+  centerY = mouseY;
+  stroke(2);
+  strokeWeight(4);
 }
 
 function draw() {
