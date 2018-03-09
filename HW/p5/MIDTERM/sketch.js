391dddf4d413280759a1c9ed24ac0862136b59ac
Marie Duchene <marie_duchene44@hotmail.com>
2018-03-08 19:02:20 -0500

    more stuff

--- HW/p5/MIDTERM/sketch.js
+++ HW/p5/MIDTERM/sketch.js
@@ -22,6 +22,15 @@ var eyebrowLPoints = [340,217];
 var eyebrowR;
 var eyebrowRPoints = [490,215];
 
+var eyeL;
+var eyeLPoints = (340,400);
+
+
+
+
+var everything = false;
+var everyButton;
+
 
 
 function preload(){
@@ -33,30 +42,56 @@ function preload(){
     front = loadImage("assets/front.png");
     eyebrowL = loadImage("assets/eyebrowL.png");
     eyebrowR = loadImage("assets/eyebrowR.png");
+    eyeL = loadImage("assets/eyeL.png");
 
 }
 
 
 function setup() {
-  // put setup code here
   createCanvas(800,800);
+
+
+
+
+  everyButton = createButton("Oh!");
+  everyButton.position(250,10);
+  everyButton.mousePressed(function(){
+
+
+        if(everything == false){
+          everything = true;
+        }else{
+          everything = false;
+        }
+
+
+      });
 }
 
+
+
 function draw() {
-  // put drawing code here
+
   background(255);
 
+if(everything == false){
   image(backgroundImage,0,0);
+}
+
 
 //LEFT CHEEK//
 ellipse(cheekLPoints[0],cheekLPoints[1], 10,10);
 
   var checkLDist = dist(mouseX,mouseY,cheekLPoints[0],cheekLPoints[1]);
-  if(checkLDist < 20){
+  if(checkLDist < 20 || everything == true){
     image(cheekL,0,0);
+    // if(everything == false){
+    //   everything = true;
+    // }else{
+    //   everything = false;
+    // }
+
 }
-
-
 
 
 
@@ -65,10 +100,9 @@ ellipse(cheekLPoints[0],cheekLPoints[1], 10,10);
   ellipse(cheekRPoints[0],cheekRPoints[1], 10,10);
 
   var checkRDist = dist(mouseX,mouseY,cheekRPoints[0],cheekRPoints[1]);
-  if(checkRDist < 40){
+  if(checkRDist < 40 || everything == true){
     image(cheekR,0,0);
 }
-//image(cheekR,0,0);
 
 
 
@@ -76,50 +110,59 @@ ellipse(cheekLPoints[0],cheekLPoints[1], 10,10);
 ellipse(mouthPoints[0], mouthPoints[1], 10,10);
 
 var checkMDist = dist(mouseX,mouseY,mouthPoints[0],mouthPoints[1]);
-if(checkMDist < 40){
+if(checkMDist < 40 || everything == true){
   image(mouth,0,0);
 }
 
-//image(mouth,0,0);
 
 
 //CHIN//
 ellipse(chinPoints[0],chinPoints[1], 10,10);
 
 var checkCHDist = dist(mouseX,mouseY,chinPoints[0],chinPoints[1]);
-if(checkCHDist < 40){
+if(checkCHDist < 40 || everything == true){
   image(chin,0,0);
 }
 
 
-//image(chin,0,0);
 
 //FRONT AND NOSE//
 ellipse(frontPoints[0],frontPoints[1],10,10);
 
 var checkFDist = dist(mouseX,mouseY,frontPoints[0],frontPoints[1]);
-if(checkFDist < 40){
+if(checkFDist < 40 || everything == true){
   image(front,0,0);
 }
+
 
 
 //LEFT EYEBROW//
 ellipse(eyebrowLPoints[0],eyebrowLPoints[1],7,7);
 
 var checkELDist = dist(mouseX,mouseY,eyebrowLPoints[0],eyebrowLPoints[1]);
-if(checkELDist <40){
+if(checkELDist <40 || everything == true){
   image(eyebrowL,0,0);
 }
+
 
 
 //RIGHT EYEBROW//
 ellipse(eyebrowRPoints[0],eyebrowRPoints[1],7,7);
 
 var checkERDist = dist(mouseX,mouseY,eyebrowRPoints[0],eyebrowRPoints[1]);
-if(checkERDist <40){
+if(checkERDist <40 || everything == true){
   image(eyebrowR,0,0);
 }
 
+
+//LEFT EYE//
+ellipse(eyeLPoints[0],eyeLPoints[1],10,10);
+
+var checkELEDist = dist(mouseX,mouseY,eyeLPoints[0],eyeLPoints[1]);
+if(checkELEDist <40 || everything == true){
+  image(eyeL,0,0);
+
+}
 
 
 
