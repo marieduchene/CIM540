3ba08c95c4c10586fdc4a742ec80283fcebae9cc
Marie Duchene <marieduchene@Maries-MacBook-Pro.local>
2018-04-12 16:09:28 -0400

    start sketch

--- HW/p5/FINAL/sketch.js
+++ HW/p5/FINAL/sketch.js
@@ -1,7 +1,79 @@
+var shape1;
+var shape2;
+
+var prevMillis = 0;
+
+var interval = 5000;
+
+var shapes = [];
+
 function setup() {
-  // put setup code here
+    createCanvas(400, 400);
+    
+
+    //shape1 = new shape(random(width), random(height), 100,200, "rect");
+    //shape2 = new shape(random(width), random(height), 100,100, "ellipse");
+
+    //shapes.push(new shape(random(width), random(height), 100,200, "rect"));
+
+    for(var i = 0; i<2; i++){
+      shapes.push(new shape(random(width), random(height), 100,200, "rect"));
+    }
+
 }
 
 function draw() {
-  // put drawing code here
+    background(255);
+    //shape1.display();
+    //shape2.display();
+    //shapes[0].display();
+
+    for(var i = 0; i<shapes.length; i++){
+      shapes[i].display();
+    }
+
+    if(millis() - prevMillis > interval){
+      //shape1.x = random(width);
+      //shape1.y = random(height);
+      for(var i = 0; i<shapes.length; i++){
+        shapes[i].scale = random(0,1.5);
+      }
+      prevMillis = millis();
+      //shape1.type = "ellipse";
+    }
+
+
+
+}
+
+function mousePressed(){
+  for(var i = 0; i<shapes.length; i++){
+    shapes[i].type = "ellipse";
+  }
+}
+
+function shape(tempX, tempY, tempWidth, tempHeight, tempType) {
+
+    this.x = tempX;
+    this.y = tempY;
+    this.width = tempWidth;
+    this.height = tempHeight;
+    this.type = tempType;
+    this.scale = 1;
+
+    this.display = function () {
+      fill(0);
+      push();
+      scale(this.scale);
+      if(this.type == "rect"){
+        rect(this.x, this.y, this.width, this.height);
+      }else if(this.type == "ellipse"){
+        ellipse(this.x, this.y, this.width, this.height);
+      }
+
+      pop();
+
+    }
+
+
 }
