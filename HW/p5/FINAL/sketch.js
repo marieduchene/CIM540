6a9e5357315bd55fefe81060dd132abeee7463fc
Marie Duchene <marieduchene@Maries-MacBook-Pro.local>
2018-04-25 15:38:56 -0400

    changes to final

--- HW/p5/FINAL/sketch.js
+++ HW/p5/FINAL/sketch.js
@@ -3,38 +3,54 @@ var shape2;
 
 var prevMillis = 0;
 
-var interval = 5000;
+var interval = 2500;
 
 var shapes = [];
 
-function setup() {
-    createCanvas(1080, 1920);
-
 var mic;
 
+var micLevel;
 
+var shapeTypes = ["rect","ellipse"];
+
+function setup() {
+    createCanvas(1080, 1920);
 
     //shape1 = new shape(random(width), random(height), 100,200, "rect");
     //shape2 = new shape(random(width), random(height), 100,100, "ellipse");
 
     //shapes.push(new shape(random(width), random(height), 100,200, "rect"));
 
-    for(var i = 0; i<2; i++){
+    for(var i = 0; i<5; i++){
       shapes.push(new shape(random(width), random(height), 100,200, "rect"));
     }
 
+    mic = new p5.AudioIn()
+    mic.start();
+    noStroke();
+
 }
 
-
-function setup(){
-  mic = new p5.AudioIn()
-  mic.start();
-}
 
 
 
 function draw() {
-    background(255);
+
+    micLevel = mic.getLevel() * 100;
+    console.log(micLevel);
+
+    if(micLevel > 4){
+      for(var i = 0; i<shapes.length; i++){
+        var curShape = int(random(0,shapeTypes.length))
+        shapes[i].type = shapeTypes[curShape];
+        shapes[i].r = random(0, 255);
+        shapes[i].g = random(0, 255);
+        shapes[i].b = random(0, 255);
+        background(random(0, 255),random(0, 255),random(0, 255));
+
+      }
+    }
+
     //shape1.display();
     //shape2.display();
     //shapes[0].display();
@@ -48,6 +64,7 @@ function draw() {
       //shape1.y = random(height);
       for(var i = 0; i<shapes.length; i++){
         shapes[i].scale = random(0,1.5); // based off of mic level
+        //shapes[i].scale = micLevel;
       }
       prevMillis = millis();
       //shape1.type = "ellipse";
@@ -71,9 +88,12 @@ function shape(tempX, tempY, tempWidth, tempHeight, tempType) {
     this.height = tempHeight;
     this.type = tempType;
     this.scale = 1;
+    this.r = 0;
+    this.g = 0;
+    this.b = 0;
 
     this.display = function () {
-      fill(0);
+      fill(this.r,this.g,this.b);
       push();
       scale(this.scale);
       if(this.type == "rect"){
@@ -89,9 +109,9 @@ function shape(tempX, tempY, tempWidth, tempHeight, tempType) {
 
 }
 
-function mousePressed() {
-  if (mouseX > 0 && mouseX < 100 && mouseY > 0 && mouseY < 100) {
-    var fs = fullscreen();
-    fullscreen(!fs);
-  }
-}
+// function mousePressed() {
+//   if (mouseX > 0 && mouseX < 100 && mouseY > 0 && mouseY < 100) {
+//     var fs = fullscreen();
+//     fullscreen(!fs);
+//   }
+// }
